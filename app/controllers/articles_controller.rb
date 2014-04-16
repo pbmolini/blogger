5812b3dbc4603560fa8a1bfcfb226470443a6026
pietro <pb@molini.me>
2014-04-16 22:42:13 +0200

    done blog comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,24 +2,26 @@ class ArticlesController < ApplicationController
    include ArticlesHelper
 
    def index
-   @articles = Article.all
+      @articles = Article.all
    end
 
    def show
-    @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
+      @comment = Comment.new
+      @comment.article_id = @article.id
    end
 
    def new
-   @article = Article.new
+      @article = Article.new
    end
 
    def create
-   @article = Article.new(article_params)
-   @article.save
+      @article = Article.new(article_params)
+      @article.save
 
-   flash.notice = "Article #{@article.title} has beed created!"
+      flash.notice = "Article #{@article.title} has beed created!"
 
-   redirect_to article_path(@article) 
+      redirect_to article_path(@article) 
    end
 
    def destroy
